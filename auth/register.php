0d6cf20e57509ad1db7dd11e4801de3752f16796
Adebisi Covenant <adebisicovenant01@gmail.com>
2026-01-19 15:37:45 +0100

    Hallelujah

--- auth/register.php
+++ auth/register.php
@@ -304,7 +304,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     </div>
 
                     <?php if ($googleAuthUrl): ?>
-                    <a href="<?php echo htmlspecialchars($googleAuthUrl); ?>" class="btn btn-google btn-auth">
+                    <a href="<?php echo htmlspecialchars($googleAuthUrl); ?>" class="btn btn-google btn-auth mb-3">
                         <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.874 2.684-6.615z" fill="#4285F4"/>
                             <path d="M9.003 18c2.43 0 4.467-.806 5.956-2.18l-2.909-2.26c-.806.54-1.836.86-3.047.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9.003 18z" fill="#34A853"/>
